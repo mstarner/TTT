30b3ab3b181f310e0a1faa889ef0920cd7b0f284
mstarner <mstarner8@gmail.com>
2017-12-18 08:26:26 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is a tic-tac-toe simulator. The board is now printed in the output. Please type in your desired player, X's or O's ");
+    }
+}
