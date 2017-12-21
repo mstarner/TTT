4685085b3c189a29e38e287c8d48c13e5b5b8f7d
Matthew Starner <mstarner8@gmail.com>
2017-12-21 08:43:07 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("This is a tic-tac-toe simulator. The board is now printed in the output. Please type in your desired player, X's or O's ");
+        String xoro = input.next();
+        String playerSide;
+        if (xoro.equals("X"))
+        {
+            playerSide = "X";
+        }
+        else (xoro.equalsIgnoreCase("O"))
+        {
+            playerSide = "O";
+        }
     }
 }
